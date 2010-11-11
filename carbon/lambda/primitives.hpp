5e3aa3390f92c25cb721867d22b6a7b12262b020
Manjunath Kudlur <keveman@gmail.com>
2010-11-11 11:40:17 -0800

    added value class

--- carbon/lambda/primitives.hpp
+++ carbon/lambda/primitives.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <carbon/utility/type_functions.hpp>
 #include <carbon/utility/vector.hpp>
 #include <carbon/lambda/actor.hpp>
 
@@ -31,5 +32,48 @@ actor<argument<0> > const _1 = argument<0>();
 actor<argument<1> > const _2 = argument<1>();
 actor<argument<2> > const _3 = argument<2>();
 actor<argument<3> > const _4 = argument<3>();
+
+template<typename T>
+struct value {
+  typedef typename carbon::utility::detail::remove_reference<T>::type plain_t;
+
+  template<typename Vec>
+  struct result {
+    typedef plain_t const type;
+  };
+
+  plain_t val;
+  
+  value(plain_t val_) : val(val_) {}
+
+  template<typename Vec>
+  __host__ __device__
+  plain_t const
+  eval(Vec const &) const {
+    return val;
+  }
+};
+
+template<typename T>
+__host__ __device__
+inline actor<value<T> > const
+val(T v)
+{
+  return value<T>(v);
+}
+
+template<typename base_t>
+void
+val(actor<base_t> const &);
+
+template<typename T>
+struct as_actor {
+  typedef actor<value<T> > type;
+
+  __host__ __device__
+  static type convert(T const& x) {
+    return value<T>(x);
+  }
+};
 
 } }
