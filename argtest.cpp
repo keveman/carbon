f7fde2ba92b8515d659a7f9ee1c3b46dccacc8fc
Manjunath Kudlur <keveman@gmail.com>
2010-11-11 21:58:47 -0800

    Fixed functions, added factorial

--- argtest.cpp
+++ argtest.cpp
@@ -2,20 +2,36 @@
 #include <carbon/lambda/primitives.hpp>
 #include <carbon/lambda/operators.hpp>
 #include <carbon/lambda/statements.hpp>
+#include <carbon/lambda/functions.hpp>
 
 using namespace std;
 using namespace carbon::lambda;
+
+struct fact_impl {
+
+  template<typename Arg>
+  struct result {
+    typedef Arg type;
+  };
+
+  template<typename Arg>
+  Arg operator()(Arg n) const {
+    return (n <= 0) ? 1 : n * this->operator()(n-1);
+  }
+};
+
+function<fact_impl> const factorial;
 
 template<typename F>
 void foo(F f)
 {
   int i=42;
-  f(i,2);
+  f(i,10);
   cout << i << "\n";
 }
 
 int main()
 {
-  foo((arg1=arg1*arg2, arg1=1, arg1 = arg1+1));
+  foo((arg1=factorial(arg2)));
   return 0;
 }
--- carbon/lambda/functions.hpp
+++ carbon/lambda/functions.hpp
@@ -26,7 +26,7 @@ struct function {
   operator()(A0 const &_a0) const {
     typedef typename detail::make_composite<op_t, A0>::composite_type
       ret_t;
-    return ret_t(op, as_actor<A0>::convert(a0));
+    return ret_t(op, as_actor<A0>::convert(_a0));
   }
 
   template<typename A0, typename A1>
@@ -35,7 +35,7 @@ struct function {
   operator()(A0 const &_a0, A1 const &_a1) const {
     typedef typename detail::make_composite<op_t, A0, A1>::composite_type
       ret_t;
-    return ret_t(op, as_actor<A0>::convert(a0), as_actor::convert(a1));
+    return ret_t(op, as_actor<A0>::convert(_a0), as_actor<A1>::convert(_a1));
   }
 
 };
