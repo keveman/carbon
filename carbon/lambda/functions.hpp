95b2be72a196ff0b0cf0228316be2f30ea3bdae5
Manjunath Kudlur <keveman@gmail.com>
2010-11-11 21:53:34 -0800

    Added functions

---
+++ carbon/lambda/functions.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <carbon/utility/config.hpp>
+#include <carbon/lambda/actor.hpp>
+#include <carbon/lambda/composite.hpp>
+
+namespace carbon { namespace lambda {
+
+template<typename op_t>
+struct function {
+
+  op_t op;
+  
+  __host__ __device__
+  function () : op() {}
+
+  __host__ __device__
+  actor<composite<op_t> >
+  operator()() const {
+    return actor<composite<op_t> >(op);
+  }
+
+  template<typename A0>
+  __host__ __device__
+  typename detail::make_composite<op_t, A0>::type
+  operator()(A0 const &_a0) const {
+    typedef typename detail::make_composite<op_t, A0>::composite_type
+      ret_t;
+    return ret_t(op, as_actor<A0>::convert(a0));
+  }
+
+  template<typename A0, typename A1>
+  __host__ __device__
+  typename detail::make_composite<op_t, A0, A1>::type
+  operator()(A0 const &_a0, A1 const &_a1) const {
+    typedef typename detail::make_composite<op_t, A0, A1>::composite_type
+      ret_t;
+    return ret_t(op, as_actor<A0>::convert(a0), as_actor::convert(a1));
+  }
+
+};
+
+} }
