1515e974b50da917b57a767e60de3005bfb4d825
Manjunath Kudlur <keveman@gmail.com>
2010-11-12 12:09:25 -0800

    simple test

--- argtest.cu
+++ argtest.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <carbon/lambda/primitives.hpp>
 #include <carbon/lambda/operators.hpp>
+#include <carbon/lambda/statements.hpp>
 
 using namespace std;
 using namespace carbon::lambda;
@@ -8,8 +9,9 @@ using namespace carbon::lambda;
 template<typename F>
 __global__ void kernel(int *a, F f)
 {
-  int i=44;
-  *a = f(42, i);
+  int i=4;
+  *a = 0;
+  f(*a, i);
 }
 
 int main()
@@ -18,7 +20,7 @@ int main()
   int *da, ha;
   cudaMalloc((void **)&da, sizeof(int));
 
-  kernel<<<1,1>>>(da, -arg2+arg1+2-arg1);
+  kernel<<<1,1>>>(da, while_(arg2)[arg2 = arg2-1, arg1 = arg1 + 1]);
 
   cudaMemcpy(&ha, da, sizeof(int), cudaMemcpyDeviceToHost);
 
