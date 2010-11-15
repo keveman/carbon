b3d3d9e5ef2ca54f2fb515a437c85ac4d6bd4ad5
Manjunath Kudlur <keveman@gmail.com>
2010-11-14 19:35:17 -0800

    Added 4 operators for function<>

--- carbon/lambda/functions.hpp
+++ carbon/lambda/functions.hpp
@@ -38,6 +38,31 @@ struct function {
     return ret_t(op, as_actor<A0>::convert(_a0), as_actor<A1>::convert(_a1));
   }
 
+  template<typename A0, typename A1, typename A2>
+  __host__ __device__
+  typename detail::make_composite<op_t, A0, A1, A2>::type
+  operator()(A0 const &_a0, A1 const &_a1, A2 const &_a2) const {
+    typedef typename detail::make_composite<op_t, A0, A1, A2>::composite_type
+      ret_t;
+    return ret_t(op,
+                 as_actor<A0>::convert(_a0),
+                 as_actor<A1>::convert(_a1),
+                 as_actor<A2>::convert(_a2));
+  }
+
+  template<typename A0, typename A1, typename A2, typename A3>
+  __host__ __device__
+  typename detail::make_composite<op_t, A0, A1, A2, A3>::type
+  operator()(A0 const &_a0, A1 const &_a1, A2 const &_a2, A3 const &_a3) const {
+    typedef typename detail::make_composite<op_t, A0, A1, A2, A3>::composite_type
+      ret_t;
+    return ret_t(op,
+                 as_actor<A0>::convert(_a0),
+                 as_actor<A1>::convert(_a1),
+                 as_actor<A2>::convert(_a2),
+                 as_actor<A3>::convert(_a3));
+  }
+
 };
 
 } }
