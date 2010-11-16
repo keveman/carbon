a552f108c0940aefce17fabc53a968786e1d9cd7
Manjunath Kudlur <keveman@gmail.com>
2010-11-15 18:44:50 -0800

    Added lambda.hpp

---
+++ carbon/lambda.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <carbon/lambda/primitives.hpp>
+#include <carbon/lambda/operators.hpp>
+#include <carbon/lambda/statements.hpp>
+#include <carbon/lambda/functions.hpp>
+#include <carbon/lambda/thrust.hpp>
