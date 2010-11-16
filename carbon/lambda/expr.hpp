3f9a1b7426ae033864ff13e4e97f9101d3efd704
Manjunath Kudlur <keveman@gmail.com>
2010-11-16 15:28:30 -0800

    Added Boost license

--- carbon/lambda/expr.hpp
+++ carbon/lambda/expr.hpp
@@ -1,3 +1,9 @@
+
+//          Copyright Manjunath Kudlur 2010 - 2012
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 #include <carbon/utility/type_functions.hpp>
 
